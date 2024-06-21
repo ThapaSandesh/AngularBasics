77ee27c4ded1dc9bf28379c4b1bcd747322138d1
Sandesh Thapa <letsmailsandesh@gmail.com>
2024-06-21 17:23:21 +0900

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'crisis-list', component: CrisisListComponent},
+{path: 'heroes-list', component: HeroesListComponent},
+];
